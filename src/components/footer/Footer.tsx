9eeb549be3645a03f9a882faaca984b0a047d3c1
Priya-Manchanda <98579874+Priya-Manchanda@users.noreply.github.com>
2023-07-26 22:03:02 +0530

    Add files via upload

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Priya</span>
+      <span>© Priya Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
